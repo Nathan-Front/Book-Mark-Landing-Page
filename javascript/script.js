cfc2084e461a7eb9139cff05127cf1af2407b708
Jonathan Tindungan <89713311+Nathan-Front@users.noreply.github.com>
2021-10-01 21:30:48 +0900

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+/*function for the burger menu*/
 const toggle = document.getElementById("burger-links");
 const menu = document.querySelector("#menu-links");
 const mobileClose = document.getElementById("mobile-close");
@@ -20,11 +21,37 @@ mobileClose.addEventListener('click', function(){
         return;
     }
 });
-/*
-function changebutton(){
-        if(document.getElementById("burger-button").src == "images/icon-close.svg"){
-            document.getElementById("burger-button").src= "images/icon-close.svg";
-        }else{
-            document.getElementById("burger-button").src= "images/icon-hamburger.svg";
-        }
-}*/
+/*function to validate the form*/
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    validateEmail();
+});
+function validateEmail(){
+    const inputEmail = email.value.trim();
+    if(inputEmail === ''){
+        sendErrorMessage(email, 'This must not be empty');
+    }else if(!emailFormat(inputEmail)){
+        sendErrorMessage(email, 'Whoops, make sure its an email');
+    }else{
+        sendSuccesfulMessage(email, 'Great job!');
+    }
+}
+function sendErrorMessage(input, message){
+    const validate = input.parentElement;
+    const span = validate.querySelector('span');
+    validate.className = 'form-input error';
+    span.innerText = message;
+}
+function sendSuccesfulMessage(input, message){
+    const validateSuccess= input.parentElement;
+    const span = validateSuccess.querySelector('span');
+    validateSuccess.className = 'form-input success';
+    span.innerText=message;
+}
+function emailFormat(email){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
